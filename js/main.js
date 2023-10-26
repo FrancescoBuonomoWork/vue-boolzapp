92c36cb8e7ab078835083e14fc95f524e4c39b8d
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-26 17:38:30 +0200

    add vue in js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log('funziona');
+
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.log('vue funziona')
+    }
+  }).mount('#app')
